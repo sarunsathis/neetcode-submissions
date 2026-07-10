da24616db1451c70e04a469dfce9bcb4ee10430c
Sarun Sathis <45716079+sarunsathis@users.noreply.github.com>
2026-07-10 22:47:28 +0530

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+
+    visitedMap = {}
+
+    def deepCopyNode(self, node: Optional['Node']) -> Optional['Node']:
+        if node.val in self.visitedMap.keys() :
+            return self.visitedMap[node.val]
+
+        newNode = Node(node.val,node.neighbors)
+        self.visitedMap[node.val] = newNode
+        neighborlist = []
+
+        for nodes in node.neighbors :
+            neighborlist.append(self.deepCopyNode(nodes))
+
+        node.neighbors = neighborlist
+        return newNode
+
+
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        self.visitedMap = {}
+        if node is None : 
+            return None
+        
+        return self.deepCopyNode(node)
+        
