f9b122381f9f3bb18cb7f04049b43d19c8ec14f5
Sarun Sathis <45716079+sarunsathis@users.noreply.github.com>
2026-08-29 11:08:04 +0530

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def backTrack(self, index: int, total: int, candidates: List[int], target:int) :
+        if total == target :
+            self.sumSet.append(self.currSubSet.copy())
+            return
+        elif total > target or index >= len(candidates) :
+            return
+
+        self.currSubSet.append(candidates[index])
+        total += candidates[index]
+        self.backTrack(index+1,total,candidates,target)
+
+        total -= self.currSubSet.pop()
+        while index < len(candidates)-1 and candidates[index] == candidates[index+1] :
+            index += 1
+            if index + 1 >= len(candidates) :
+                return
+        self.backTrack(index+1,total,candidates,target)
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.currSubSet = []
+        self.sumSet = []
+        # self.seenSet = set()
+        candidates.sort()
+
+        self.backTrack(0,0,candidates,target)
+        return self.sumSet
+        
